8b1cb3c5838d86ad279b92b464cfe8629867f218
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-11 21:41:33 -0300

    created handleStyle logic

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const controles = document.getElementById("controles");
 const cssText = document.querySelector(".css");
-const button = document.querySelector(".btn");
+const btn = document.querySelector(".btn");
 controles.addEventListener("change", handleChange);
 
-function handleChange(event) {}
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + "px";
+  },
+  width(value) {
+    this.element.style.width = value + "px";
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + "px";
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + "rem";
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+  border(value) {
+    this.element.style.border = border;
+  },
+};
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  handleStyle[name](value);
+}
