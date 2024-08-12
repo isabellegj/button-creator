8f85b3f7e83dda8cbf781bd9d45c0fb4ab923f20
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-11 21:19:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const controles = document.getElementById("controles");
+const cssText = document.querySelector(".css");
+const button = document.querySelector(".btn");
+controles.addEventListener("change", handleChange);
+
+function handleChange(event) {}
